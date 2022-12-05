cd7226af916d53bd2b0dc9f3671b52b363b64eb7
Marcus Ayers <marcayers27@gmail.com>
2022-12-05 16:08:58 -0500

    all 5 tests passing

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_05_210307) do
+ActiveRecord::Schema.define(version: 2022_12_05_210619) do
 
   create_table "tasks", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "content"
+    t.boolean "completed", default: false
   end
 
 end
