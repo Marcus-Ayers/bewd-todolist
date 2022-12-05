c075023e52788c71ec1f6d1ba8e673709b06b1d9
Marcus Ayers <marcayers27@gmail.com>
2022-12-05 16:13:05 -0500

    5 tests passing

--- db/migrate/20221205210619_add_attributes_to_tasks.rb
+++ db/migrate/20221205210619_add_attributes_to_tasks.rb
@@ -2,6 +2,5 @@ class AddAttributesToTasks < ActiveRecord::Migration[6.1]
   def change
     add_column :tasks, :content, :string
     add_column :tasks, :completed, :boolean, default: false
-    #h
   end
 end
